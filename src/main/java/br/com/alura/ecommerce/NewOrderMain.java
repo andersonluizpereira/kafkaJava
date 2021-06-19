25a4509c33d0243fe40b18cffafe09526cf87339
Anderson Pereira <andersononus@hotmail.com>
2021-06-19 18:48:08 -0300

    add log serializer

--- src/main/java/br/com/alura/ecommerce/NewOrderMain.java
+++ src/main/java/br/com/alura/ecommerce/NewOrderMain.java
@@ -1,22 +1,26 @@
 package br.com.alura.ecommerce;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 public class NewOrderMain {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        try (var dispatcher = new KafkaDispatcher()) {
-            for (var i = 0; i < 10; i++) {
+        try (var orderKafkaDispatcher = new KafkaDispatcher<Order>()) {
+            try (var emailKafkaDispatcher = new KafkaDispatcher<String>()) {
+                for (var i = 0; i < 10; i++) {
 
-                var key = UUID.randomUUID().toString();
-                var value = key + ",67523,1234";
-                dispatcher.send("ECOMMERCE_NEW_ORDER", key, value);
+                    var userId = UUID.randomUUID().toString();
+                    var orderId = UUID.randomUUID().toString();
+                    var amount = new BigDecimal(Math.random() * 5000 + 1);
+                    var order = new Order(userId, orderId, amount);
+                    orderKafkaDispatcher.send("ECOMMERCE_NEW_ORDER", userId, order);
 
-                var email = "Thank you for your order! We are processing your order!";
-                dispatcher.send("ECOMMERCE_SEND_EMAIL", key, email);
+                    var email = "Thank you for your order! We are processing your order!";
+                    emailKafkaDispatcher.send("ECOMMERCE_SEND_EMAIL", userId, email);
+                }
             }
         }
     }
-
 }
---
+++ start.sh
@@ -0,0 +1,2 @@
+docker stop c3900d06fc0a f74ccdb36a6b 8cb26bf3c0f7
+docker start c3900d06fc0a f74ccdb36a6b 8cb26bf3c0f7
