0c15c4b3c2ee29107c746bf53f86bc7c9f25fae5
Anderson Pereira <andersononus@hotmail.com>
2021-06-22 11:03:35 -0300

    adjust query

--- service-new-order/src/main/java/br/com/alura/ecommerce/NewOrderMain.java
+++ service-new-order/src/main/java/br/com/alura/ecommerce/NewOrderMain.java
@@ -1,29 +1,65 @@
 package br.com.alura.ecommerce;
 
-import java.math.BigDecimal;
-import java.util.UUID;
-import java.util.concurrent.ExecutionException;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 
-public class NewOrderMain {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Map;
 
-    public static void main(String[] args) throws ExecutionException, InterruptedException {
-        try (var orderDispatcher = new KafkaDispatcher<Order>()) {
-            try (var emailDispatcher = new KafkaDispatcher<String>()) {
-                var email = Math.random() + "@email.com";
-                for (var i = 0; i < 10; i++) {
+class CreateUserService {
 
-                    var userId = UUID.randomUUID().toString();
-                    var orderId = UUID.randomUUID().toString();
-                    var amount = new BigDecimal(Math.random() * 5000 + 1);
+    private final Connection connection;
 
-                    var order = new Order(userId, orderId, amount, email);
-                    orderDispatcher.send("ECOMMERCE_NEW_ORDER", userId, order);
-
-                    var emailCode = "Thank you for your order! We are processing your order!";
-                    emailDispatcher.send("ECOMMERCE_SEND_EMAIL", userId, emailCode);
-                }
-            }
+    CreateUserService() throws SQLException {
+        String url = "jdbc:sqlite:target/users_database.db";
+        connection = DriverManager.getConnection(url);
+        try {
+            connection.createStatement().execute("create table Users (" +
+                    "uuid varchar(200) primary key," +
+                    "email varchar(200))");
+        } catch(SQLException ex) {
+            // be careful, the sql could be wrong, be reallllly careful
+            ex.printStackTrace();
         }
+    }
+
+    public static void main(String[] args) throws SQLException {
+        var createUserService = new CreateUserService();
+        try (var service = new KafkaService<>(CreateUserService.class.getSimpleName(),
+                "ECOMMERCE_NEW_ORDER",
+                createUserService::parse,
+                Order.class,
+                Map.of())) {
+            service.run();
+        }
+    }
+
+    private void parse(ConsumerRecord<String, Order> record) throws SQLException {
+        System.out.println("------------------------------------------");
+        System.out.println("Processing new order, checking for new user");
+        System.out.println(record.value());
+        var order = record.value();
+        if(isNewUser(order.getEmail())) {
+            insertNewUser(order.getUserId(), order.getEmail());
+        }
+    }
+
+    private void insertNewUser(String uuid, String email) throws SQLException {
+        var insert = connection.prepareStatement("insert into Users (uuid, email) " +
+                "values (?,?)");
+        insert.setString(1, uuid);
+        insert.setString(2, email);
+        insert.execute();
+        System.out.println("Usuário uuid e " + email + " adicionado");
+    }
+
+    private boolean isNewUser(String email) throws SQLException {
+        var exists = connection.prepareStatement("select uuid from Users " +
+                "where email = ? limit 1");
+        exists.setString(1, email);
+        var results = exists.executeQuery();
+        return !results.next();
     }
 
 }
